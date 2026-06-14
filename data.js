f341fcf21ff31ee2ed5d84a43da8a9ae879539b9
annastasiameshcheryakova-create <annastasia.meshcheryakova@gmail.com>
2026-06-14 11:30:42 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const socialData = {
+
+    nodes:[
+        {id:1,label:"Анна"},
+        {id:2,label:"Олег"},
+        {id:3,label:"Марія"},
+        {id:4,label:"Іван"},
+        {id:5,label:"Софія"}
+    ],
+
+    edges:[
+        {from:1,to:2},
+        {from:1,to:3},
+        {from:2,to:4},
+        {from:3,to:4},
+        {from:4,to:5}
+    ]
+};
+
+const transportData = {
+
+    nodes:[
+        {id:1,label:"Київ"},
+        {id:2,label:"Львів"},
+        {id:3,label:"Одеса"},
+        {id:4,label:"Харків"},
+        {id:5,label:"Дніпро"}
+    ],
+
+    edges:[
+        {from:1,to:2},
+        {from:1,to:3},
+        {from:1,to:4},
+        {from:4,to:5},
+        {from:3,to:5}
+    ]
+};
